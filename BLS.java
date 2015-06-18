c4b9c164895449aca14c29c7fcaa982415083bee
NeedsMoreGravitas <dyshallow@gmail.com>
2015-06-18 17:02:22 -0500

    more cleanup, collapsed some code sections (not sure if those transfer to different IDEs), also did some initial experimentation into getting the original hash back from the mapping

--- BLS.java
+++ BLS.java
@@ -2,8 +2,8 @@
  * Created by Dylan on 6/16/2015.
  *
  *
- *
- * Needs cleaning...
+ * Azhar-
+ * see line 50
  */
 
 
@@ -14,25 +14,26 @@ import org.bouncycastle.jce.provider.JDKMessageDigest;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
+import java.util.Arrays;
 
 public class BLS {
 
     public static void main(String args[]){
 
-        // setup
+        // setup pairing
         Pairing pairing = PairingFactory.getPairing("a.properties");    // curve parameters
         Element g = pairing.getG1().newRandomElement().getImmutable();  // system parameters
         Element privateKey = pairing.getZr().newRandomElement();        // secret/private key
         Element pK = g.powZn(privateKey);                               // public key
 
-        // set values "messages"
+        // set values: "messages"
         BigInteger m = new BigInteger("6");
         BigInteger n = new BigInteger("7");
         BigInteger c = m.add(n); // combined value
 
         Element cSig = generateSignature(c, privateKey, pairing);
 
-        Element s = mapValue(c, pairing);
+        //Element s = mapValue(c, pairing);
 
         if(verifySignature(cSig, g, c, pK, pairing)){
             System.out.println("verified");
@@ -40,10 +41,54 @@ public class BLS {
             System.out.println("not valid");
         }
 
+        // begin recovery testing
+        System.out.println("private key:");
+        System.out.println(Arrays.toString(privateKey.toBytes()));
+        System.out.println("");
+        BigInteger testValue = new BigInteger("0");
+        Element map = mapValue(testValue, pairing);
+        Element signature = map.powZn(privateKey); // How can they be equal!? *********************
+
+        System.out.println(Arrays.toString(map.toBytes()));
+        System.out.println(Arrays.toString(signature.toBytes()));
+
+        System.out.println(map.hashCode());
+        System.out.println(signature.hashCode());
+
+        System.out.println(map);
+        System.out.println(signature);
+
+        if (map.equals(signature))
+        {
+            System.out.println("they are equal");
+        } else  {
+            System.out.println("they're not equal");
+        }
+        if(map.isEqual(signature)){
+            System.out.println("they are equal");
+        } else {
+            System.out.println("they're not equal");
+        }
 
 
-        System.exit(0);//**************
-        //((new BigInteger(mHash)).multiply(new BigInteger(nHash))).toByteArray();
+
+
+
+
+
+        /*
+            attempt to reverse signature process (?)
+
+            turns out that Sout(byteArray) gives you the pointer instead of the array. Or something like that anyway :|
+                use Arrays.toString(byteArray)
+
+
+
+        */
+
+        System.exit(0); // exit
+        // collapsed code block (also commented out):
+        /*//((new BigInteger(mHash)).multiply(new BigInteger(nHash))).toByteArray();
 
 
         // try using value instead of hash
@@ -72,14 +117,14 @@ public class BLS {
         Element nTemp1 = pairing.pairing(sigN, g);
         Element nTemp2 = pairing.pairing(k,pK);
 
-       /* if(mTemp1.equals(mTemp2)){
+       *//* if(mTemp1.equals(mTemp2)){
             System.out.println("m verified");
             System.out.println(sigM);
         }
         if(nTemp1.equals(nTemp2)){
             System.out.println("n verified");
             System.out.println(sigN);
-        }*/
+        }*//*
 
         Element v = j.mul(k);
 
@@ -94,18 +139,19 @@ public class BLS {
 
 
 
-        /*if(cTemp1.equals(cTemp2)){
+        *//*if(cTemp1.equals(cTemp2)){
             System.out.println("combined sig verified");
             System.out.println(sigCombined);
         } else{
             System.out.println("combined sig failed");
-        }*/
+        }*//*
 
 
         // multiply mapping and signature together, and it will verify for the messages added together
-
+        */
     }
 
+    // verifies that the signature is valid, given the necessary inputs
     public static boolean verifySignature(Element signature, Element sysParams, BigInteger message, Element publicKey, Pairing pairing){
         Element map = mapValue(message, pairing);
         Element temp1 = pairing.pairing(signature, sysParams);
@@ -116,6 +162,7 @@ public class BLS {
         return false;
     }
 
+    // generates the signature using a value and the private key (uses hash of value)
     public static Element generateSignature(BigInteger value, Element privateKey, Pairing pairing) {
         Element sig;
         Element map = mapValue(value, pairing);
@@ -123,6 +170,7 @@ public class BLS {
         return sig;
     }
 
+    // takes the value, and maps the hash of the value
     public static Element mapValue(BigInteger value, Pairing pairing){
         Element map;
         byte[] hash = hash(value);
@@ -130,10 +178,11 @@ public class BLS {
         return map;
     }
 
+    // computes hash of value with SHA256
     public static byte[] hash(BigInteger value){
         byte[] hash;
         byte[] temp = value.toByteArray();
-        MessageDigest md = new JDKMessageDigest.MD5();
+        MessageDigest md = new JDKMessageDigest.SHA256();
         hash = md.digest(temp);
         return hash;
     }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # BLSSignature
+# implements basic BLS functionality
+# will be used to investigate BLS homomorphism 
+# and other techniques that may apply to access control and provable data possession
